4c2f2f27e015dd64e9ace046b1f22fb3ecd22683
KenBan09 <keeno.pkt@gmail.com>
2025-01-28 15:40:41 +0700

    lab15_4

--- lab15_4.cpp
+++ lab15_4.cpp
@@ -19,3 +19,21 @@ int main(){
 }
 
 //Do not modify source code above this line
+
+void shuffle(int &a,int &b,int &c,int &d){
+	int arr[4] ={ a, b, c, d};
+	int temp;
+	for( int j = 0; j<4 ; j++){
+		int ran = rand()%4;
+		temp = arr[j];
+		arr[j] = arr[ran];
+		arr[ran] = temp;
+		
+	}
+	a = arr[0];
+	b= arr[1];
+	c = arr[2];
+	d = arr[3];
+
+
+}
